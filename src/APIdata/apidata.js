ea34a94b359cacbc86fd45ae1a8f12b43d0bc337
Adhish Borad <59970991+Adhish184@users.noreply.github.com>
2021-04-12 23:30:15 +0530

    Added demo data for further development

---
+++ src/APIdata/apidata.js
@@ -0,0 +1,54 @@
+
+
+export default [
+    {
+      id: 11111,
+      title: "Title 1",
+      abstract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      author: "Author 1",
+      date: "Date 1",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare arcu dui vivamus arcu felis bibendum ut. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Felis eget nunc lobortis mattis aliquam faucibus purus in. Pharetra convallis posuere morbi leo. Hendrerit dolor magna eget est lorem ipsum. Purus non enim praesent elementum facilisis. Varius morbi enim nunc faucibus. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Nunc non blandit massa enim nec. Ipsum suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Id donec ultrices tincidunt arcu. Leo in vitae turpis massa sed elementum tempus. Turpis egestas pretium aenean pharetra magna ac. Sagittis orci a scelerisque purus semper eget duis. Magnis dis parturient montes nascetur ridiculus. Diam quam nulla porttitor massa id neque aliquam vestibulum morbi."
+
+    },
+
+    {
+        id: 22222,
+        title: "Title 2",
+        abstract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        author: "Author 2",
+        date: "Date 2",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare arcu dui vivamus arcu felis bibendum ut. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Felis eget nunc lobortis mattis aliquam faucibus purus in. Pharetra convallis posuere morbi leo. Hendrerit dolor magna eget est lorem ipsum. Purus non enim praesent elementum facilisis. Varius morbi enim nunc faucibus. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Nunc non blandit massa enim nec. Ipsum suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Id donec ultrices tincidunt arcu. Leo in vitae turpis massa sed elementum tempus. Turpis egestas pretium aenean pharetra magna ac. Sagittis orci a scelerisque purus semper eget duis. Magnis dis parturient montes nascetur ridiculus. Diam quam nulla porttitor massa id neque aliquam vestibulum morbi."
+  
+    },
+
+    {
+        id: 33333,
+        title: "Title 3",
+        abstract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        author: "Author 3",
+        date: "Date 3",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare arcu dui vivamus arcu felis bibendum ut. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Felis eget nunc lobortis mattis aliquam faucibus purus in. Pharetra convallis posuere morbi leo. Hendrerit dolor magna eget est lorem ipsum. Purus non enim praesent elementum facilisis. Varius morbi enim nunc faucibus. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Nunc non blandit massa enim nec. Ipsum suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Id donec ultrices tincidunt arcu. Leo in vitae turpis massa sed elementum tempus. Turpis egestas pretium aenean pharetra magna ac. Sagittis orci a scelerisque purus semper eget duis. Magnis dis parturient montes nascetur ridiculus. Diam quam nulla porttitor massa id neque aliquam vestibulum morbi."
+  
+    },
+
+
+    {
+        id: 44444,
+        title: "Title 4",
+        abstract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        author: "Author 4",
+        date: "Date 4",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare arcu dui vivamus arcu felis bibendum ut. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Felis eget nunc lobortis mattis aliquam faucibus purus in. Pharetra convallis posuere morbi leo. Hendrerit dolor magna eget est lorem ipsum. Purus non enim praesent elementum facilisis. Varius morbi enim nunc faucibus. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Nunc non blandit massa enim nec. Ipsum suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Id donec ultrices tincidunt arcu. Leo in vitae turpis massa sed elementum tempus. Turpis egestas pretium aenean pharetra magna ac. Sagittis orci a scelerisque purus semper eget duis. Magnis dis parturient montes nascetur ridiculus. Diam quam nulla porttitor massa id neque aliquam vestibulum morbi."
+  
+    },
+
+    {
+        id: 55555,
+        title: "Title 5",
+        abstract: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        author: "Author 5",
+        date: "Date 5",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare arcu dui vivamus arcu felis bibendum ut. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae. Felis eget nunc lobortis mattis aliquam faucibus purus in. Pharetra convallis posuere morbi leo. Hendrerit dolor magna eget est lorem ipsum. Purus non enim praesent elementum facilisis. Varius morbi enim nunc faucibus. Aliquet porttitor lacus luctus accumsan tortor posuere ac ut. Nunc non blandit massa enim nec. Ipsum suspendisse ultrices gravida dictum fusce ut placerat orci nulla. Id donec ultrices tincidunt arcu. Leo in vitae turpis massa sed elementum tempus. Turpis egestas pretium aenean pharetra magna ac. Sagittis orci a scelerisque purus semper eget duis. Magnis dis parturient montes nascetur ridiculus. Diam quam nulla porttitor massa id neque aliquam vestibulum morbi."
+  
+    },
+]
