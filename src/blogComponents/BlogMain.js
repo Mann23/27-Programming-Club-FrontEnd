e2ea1a257351d351bf8a86d63deba2f3ffac074a
Adhish Borad <59970991+Adhish184@users.noreply.github.com>
2021-04-17 23:37:16 +0530

    Updated the blog-card component

--- src/blogComponents/BlogMain.js
+++ src/blogComponents/BlogMain.js
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import MediaCard from './blog-card.js'
+import BlogCard from './blog-card.js'
 import ViewBlog from './view-blog.js';
 import CreateBlog from './create-blog.js';
 import apidata from '../APIdata/apidata.js'
@@ -31,10 +31,14 @@ export default class BlogMain extends React.Component {
                     direction="row"
                     justify="space-evenly"
                     alignItems="flex-start">
-                    <Grid item xl={3}> <MediaCard /> </Grid>
-                    <Grid item xl={3}> <MediaCard /> </Grid>
-                    <Grid item xl={3}> <MediaCard /> </Grid>
-                    <Grid item xl={3}> <MediaCard /> </Grid>
+                    <Grid item xl={3}> <BlogCard /> </Grid>
+                    <Grid item xl={3}> <BlogCard /> </Grid>
+                    <Grid item xl={3}> <BlogCard /> </Grid>
+                    <Grid item xl={3}> <BlogCard /> </Grid>
+                    <Grid item xl={3}> <BlogCard /> </Grid>
+                    <Grid item xl={3}> <BlogCard /> </Grid>
+                    <Grid item xl={3}> <BlogCard /> </Grid>
+                    <Grid item xl={3}> <BlogCard /> </Grid>
                 </Grid>
 
 
--- src/blogComponents/blog-card.js
+++ src/blogComponents/blog-card.js
@@ -7,6 +7,7 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+// eslint-disable-next-line
 import sampleCardImage from "./images/sampleCardImage.jpg";
 
 const useStyles = makeStyles({
@@ -15,14 +16,16 @@ const useStyles = makeStyles({
         marginTop: "5%",
         marginRight: "2%",
         marginLeft: "2%",
-        marginBottom: "5%"
+        marginBottom: "5%",
+        // boxShadow: "0 2.8px 2.2px rgba(0, 0, 0, 0.034), 0 6.7px 5.3px rgba(0, 0, 0, 0.048), 0 12.5px 10px rgba(0, 0, 0, 0.06), 0 22.3px 17.9px rgba(0, 0, 0, 0.072), 0 41.8px 33.4px rgba(0, 0, 0, 0.086), 0 100px 80px rgba(0, 0, 0, 0.12)"
+        boxShadow: "rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px",
     },
     media: {
-        height: 140,
+        height: 30,
     },
 });
 
-export default function MediaCard() {
+export default function BlogCard() {
     const classes = useStyles();
 
     return (
@@ -31,15 +34,15 @@ export default function MediaCard() {
 
                 <CardMedia
                     className={classes.media}
-                    image={sampleCardImage}
-                    title="Contemplative Reptile"
+                    // image={sampleCardImage}
+                    title=""
                 />
 
                 <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
+                    <Typography gutterBottom variant="h4" component="h5">
                         Lizard
                     </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
+                    <Typography variant="body1" color="textSecondary" component="p">
                         Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
                         across all continents except Antarctica
                     </Typography>
@@ -47,7 +50,7 @@ export default function MediaCard() {
             </CardActionArea>
 
             <CardActions>
-                <Button size="small" color="primary" variant="outlined" >
+                <Button size="small" color="primary" variant="outlined">
                     Learn More
                 </Button>
                 <Button size="small" color="primary">
