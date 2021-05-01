eb52e448134fde980abb20bc42852767f4951769
Mann23 <manngondaliya@gmail.com>
2021-05-01 12:32:19 +0530

    Redirect to login if user not logged in

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import BlogMain from './blogComponents/BlogMain';
-import './index.css';
 import React, { lazy, Suspense } from 'react'
+import {  Route ,Switch} from 'react-router-dom'
+import {Grid, makeStyles} from "@material-ui/core"
+
+import './index.css';
 import "./App.css";
+
+import Login from './components/Login/Login';
 import ChatBody from "./components/chatBody/ChatBody";
 import Header from './components/Header/Header'
 import AllEvents from './components/TimeTable/AllEvents'
-import Login from './components/Login/Login';
-import AllEventsTrial from './components/TimeTable/AllEventsTrial'
 
-import { BrowserRouter as Router,Route ,Switch} from 'react-router-dom'
-import {Grid, makeStyles} from "@material-ui/core"
+import CreateBlog from "./blogComponents/create-blog.js";
+import ViewBlog from "./blogComponents/view-blog.js";
+import EditBlog from "./blogComponents/edit-blog.js";
+import BlogMain from './blogComponents/BlogMain';
+
+import history from './history';
+
+
 // https://www.freecodecamp.org/news/react-router-tutorial/#:~:text=To%20add%20the%20link%20in,link%20if%20it%20is%20active.
 const useStyles = makeStyles((theme) => ({
   toolbarUncover: theme.mixins.toolbar,
@@ -17,47 +25,42 @@ const useStyles = makeStyles((theme) => ({
 
 localStorage.setItem('UserID',201801454);
 
-function App() {
+function App(){
   const {toolbarUncover} = useStyles();
   return (
 
-  <div>
-    <Router>
-      <Grid container direction={"column"}>
-        <Grid item xs={12} spacing={10}>
+  <div>  
+      <Grid container direction={"column"} spacing={10}>
+        <Grid item xs={12} >
           <Header/>
           <div className={toolbarUncover} />
         </Grid>
-        <Grid item xs={12}>
-          <Switch>  
-            <Route path="/events">
-              <AllEvents />
-            </Route>
+        <Grid item xs={12}>  
+          <Switch> 
             
-            <Route path="/login">
-              <Login />
-            </Route>
+            { localStorage.getItem('UserID') ? ( 
+              <>
+              <Route path="/login" component={Login} exact/>
+            <Route  path="/events" component={AllEvents} exact/>
             <Route path="/chat">
               <div className="__main">
                 <ChatBody />
               </div>
             </Route>
-            <Route path="/blog">
-              
-      {/* {<BlogMain />} */}
-            </Route>
-
-            <Route path="/">
-            <Route />
-
+            <Route path="/view-blog" component={ViewBlog} exact/>
+            <Route path="/create-blog" component={CreateBlog} exact/>
+            <Route path="/edit-blog/:id" component={EditBlog} exact/>
+            <Route path="/" component={BlogMain} exact/>
+            </>
+            ): (<>
+              <Route  path="/" component={Login} />
+            </>)}
           </Switch>
         </Grid>
       </Grid>
-    </Router>
-
-
   </div>
   );
+
 }
 
 export default App;
